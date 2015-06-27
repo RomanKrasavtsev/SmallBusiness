6a116464d7b514bdc085d951cd9cefb43ac46a83
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2015-06-27 20:26:38 +0300

    Update dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
-  	@clients = Client.order(:description)
+  	@clients = Client.order("description").all
+  	@clients_months = @clients.group_by { |c| c.strftime("%B %Y") }
   end
 end
