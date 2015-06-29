da30c083bebc140edd70944bf0bef3ead618ccff
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2015-06-30 01:36:08 +0300

    Update Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,8 @@ class DashboardController < ApplicationController
     this_year_clients = Client.where( created_at: Time.now.beginning_of_year..Time.now.end_of_year.midnight )
     clients_by_months = this_year_clients.group_by { |c| c.created_at.beginning_of_month }
 
-    @clients_months = [{ "name": "Planned",
+    @clients_months = [{
+                        "name": "Planned",
                         "data": {
                             'January 2015':   1,
                             'February 2015':  2,
@@ -16,9 +17,11 @@ class DashboardController < ApplicationController
                             'September 2015': 9,
                             'October 2015':   10,
                             'November 2015':  11,
-                            'December 2015':  12 
+                            'December 2015':  12
                         }}, 
-                        { "name": "Real", "data": {} }]
+                        { 
+                            "name": "Real", "data": {} 
+                        }]
     
     clients_by_months.keys.sort.each do |month|
         @clients_months[1][:data][month.strftime("%B %Y")] = clients_by_months[month].size
